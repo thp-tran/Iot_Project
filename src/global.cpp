4c7f69eca7047a2485b1347b51083355bd20718f
Quang Tran <quang.tran2005@hcmut.edu.vn>
2025-12-03 17:21:14 +0700

    Finish tiny ML

--- src/global.cpp
+++ src/global.cpp
@@ -10,6 +10,7 @@ String ssid = "ESP32-YOUR NETWORK HERE!!!";
 String password = "12345678";
 String wifi_ssid = "abcde";
 String wifi_password = "123456789";
+boolean isWifiConnected = false;
 SemaphoreHandle_t xBinarySemaphoreInternet = xSemaphoreCreateBinary();
 TaskHandle_t blinkTaskHandle = xTaskGetCurrentTaskHandle();
 
